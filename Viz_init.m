b3523eb3b347f2026fe44d48825ec9d13e3d30ca
olyakrylo <olyakrylo24@gmail.com>
2020-11-30 19:28:15 +0300

    add image and ship graphs

--- Viz_init.m
+++ Viz_init.m
@@ -1,27 +1,37 @@
-% Пароход Northern Clipper
-% Эксперименты с визуальным управлением
-% Вначале открой posi_vizual,
-% а затем запусти этот файл!
-% Он настроит и запустит модель!
+% Ship Northern Clipper
+% Visual control experiments
+% First, open posi_vizual,
+% then run this file!
+% It will set and run the model!
 
 clear all;
 
 global m L m11 m22 m33 d11 d22 d33 d23 d32;
 
-% Задание параметров парохода
-m  = 4.591e6;                % Масса в кг
-L  = 76.2;                   % Длина в м 
-% Матрица и параметры инерции
-M  = diag([5.3122e6  8.2831e6 3.7454e9]); 
+% Ship parameters setting
+m = 4.591e6;                % Weight [kg]
+L = 76.2;                   % Length [m]
+
+d = 1e4*[1 2 10];
+tau = [0 0 0];
+
+% Inertial matrix and parameters
+M = diag([5.3122e6  8.2831e6 3.7454e9]); 
 M1 = inv(M);
-m11 = M1(1,1); m22=M1(2,2); m33=M1(3,3);
-% Матрица и параметры демпфирования
+m11 = M1(1,1);
+m22 = M1(2,2);
+m33 = M1(3,3);
+
+% Damping matrix and parameters 
 D  = [5.0242e4      0          0;
           0      2.7229e5  -4.3933e6;
           0     -4.3933e6   4.1894e8];
-D1=M1*D;
-d11=D1(1,1);  d22=D1(2,2); d23=D1(2,3);  
-              d32=D1(3,2); d33=D1(3,3);
+D1 = M1 * D;
+d11 = D1(1,1);
+d22 = D1(2,2);
+d23 = D1(2,3);  
+d32 = D1(3,2);
+d33 = D1(3,3);
 
-% Старт модели
+% Model start
 sim('posi_vizual');
